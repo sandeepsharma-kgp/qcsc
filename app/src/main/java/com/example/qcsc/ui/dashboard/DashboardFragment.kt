0bde5ede449c64303cb3d6d938a6a71c9142f411
sandeepsharma-kgp <sandeepsharma.iit@gmail.com>
2020-03-06 00:42:50 +0530

    trying for read from a file in json

--- app/src/main/java/com/example/qcsc/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/qcsc/ui/dashboard/DashboardFragment.kt
@@ -1,23 +1,32 @@
 package com.example.qcsc.ui.dashboard
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.RadioButton
-import android.widget.RadioGroup
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.qcsc.EOAModel
+import com.example.qcsc.PVConstants
+import com.example.qcsc.PVConstants.PVA
 import com.example.qcsc.R
 import com.example.qcsc.ui.Values
+import com.google.api.services.sheets.v4.Sheets
+import org.json.JSONObject
+import java.io.IOException
+import java.time.temporal.ValueRange
+
 
 class DashboardFragment : Fragment() {
 
+    private var result: com.google.api.services.sheets.v4.model.ValueRange? = null
     private lateinit var dashboardViewModel: DashboardViewModel
-    private lateinit var listView: ListView
+    private lateinit var pva: TextView
+    private lateinit var pvh: TextView
+    private lateinit var zScore: TextView
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,44 +35,14 @@ class DashboardFragment : Fragment() {
     ): View? {
         dashboardViewModel =
             ViewModelProviders.of(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val radioGroup: RadioGroup = root.findViewById(R.id.company)
-        listView = root.findViewById(R.id.list_view)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        pva = root.findViewById(R.id.pva_tv)
+        pvh = root.findViewById(R.id.pvh_tv)
+        zScore = root.findViewById(R.id.zscore_tv)
 
-        radioGroup.setOnCheckedChangeListener() { _radioGroup: RadioGroup, i: Int ->
-            val radioButton: RadioButton = root.findViewById(i)
-            val txt = radioButton.text
-            var CompanyMap: Map<Int, Float> = when (txt) {
-                "SJMRegent" -> mapOf<Int, Float>(17 to 1.30f, 19 to 1.70f, 21 to 2.00f, 23 to 2.50f)
-
-                "SorinBicarbon" -> mapOf<Int, Float>(19 to 0.80f, 21 to 2.10f, 23 to 1.60f)
-
-                "SJMHPMasters" -> mapOf<Int, Float>(
-                    17 to 1.00f,
-                    19 to 1.30f,
-                    21 to 1.60f,
-                    23 to 1.80f
-                )
-
-                "TTKChitraStd" -> mapOf<Int, Float>(
-                    17 to 0.92f,
-                    19 to 1.21f,
-                    21 to 1.52f,
-                    23 to 1.91f
-                )
-
-                "SJMEPIC" -> mapOf<Int, Float>(19 to 1.20f, 21 to 1.4f, 23 to 1.7f)
-
-                "PERIMAGNA" -> mapOf<Int, Float>(19 to 1.58f, 21 to 1.9f, 23 to 2.07f, 25 to 2.33f)
-
-                else -> mapOf<Int, Float>(19 to 1.28f, 21 to 1.69f, 23 to 1.87f, 25 to 1.89f)
-            }
-            val lst = CompanyMap.mapValues { Values.bsa?.let { it1 -> it.value.div(it1) } }
-            val listItems = mutableListOf<EOAModel>()
-            lst.forEach{ listItems.add(EOAModel(it.key,it.value)) }
-            listView.adapter =
-                activity?.let { ArrayAdapter(it,android.R.layout.simple_expandable_list_item_1,listItems) }
-        }
+        pva.text = PVConstants.PVA.toString()
+        pvh.text = PVConstants.PVH.toString()
+        val json = JSONObject("/src/com/example/qcsc/assets/PVA.json")
         return root
     }
 }
--- app/src/main/java/com/example/qcsc/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/qcsc/ui/dashboard/DashboardViewModel.kt
@@ -1,10 +1,16 @@
 package com.example.qcsc.ui.dashboard
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.api.services.sheets.v4.Sheets
+import java.io.IOException
+import java.time.temporal.ValueRange
 
 class DashboardViewModel : ViewModel() {
+    private lateinit var service: Sheets
+    private var result: com.google.api.services.sheets.v4.model.ValueRange? = null
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
